6190f01b187c2138c016e080b9732f35103333f0
dyama <dyama@member.fsf.org>
2014-08-25 17:47:00 +0900

    Update test script.

--- test/mruby-siren.rb
+++ test/mruby-siren.rb
@@ -6,10 +6,14 @@ puts "--------------"
 
 sp = Vec.new(5.0, 1.2, 3.8)
 tp = Vec.new(8.0, 4.2, 3.8)
-
 a = Build.line(sp, tp)
-BRepIO.save(a, "./hoge.brep")
 
-b = BRepIO.load("./hoge.brep");
-p b.shapetype
+b = Build.vertex(1, 2, 3)
+
+c = Build.compound([a, b])
+
+BRepIO.save(c, "/tmp/hoge.brep")
+
+cc = BRepIO.load("/tmp/hoge.brep");
+p cc.shapetype
 
