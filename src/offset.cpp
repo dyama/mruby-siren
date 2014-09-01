3af689fc434d0a2a43094e91628733f26737ec1b
dyama <dyama@member.fsf.org>
2014-09-01 13:57:13 +0900

    Add sweep_path method to Offset module.

--- src/offset.cpp
+++ src/offset.cpp
@@ -4,6 +4,7 @@ bool siren_offset_install(mrb_state* mrb, struct RClass* rclass)
 {
   rclass = mrb_define_module(mrb, "Offset");
   mrb_define_class_method(mrb, rclass, "sweep_vec", siren_offset_sweep_vec, ARGS_REQ(2));
+  mrb_define_class_method(mrb, rclass, "sweep_path", siren_offset_sweep_path, ARGS_REQ(2) | ARGS_OPT(4));
   return true;
 }
 
@@ -27,5 +28,90 @@ mrb_value siren_offset_sweep_vec(mrb_state* mrb, mrb_value self)
   *shape = mp.Shape();
 
   return siren_shape_new(mrb, shape);
+}
+
+mrb_value siren_offset_sweep_path(mrb_state* mrb, mrb_value self)
+{
+
+  mrb_value target, pathwire;
+  mrb_value cont, corr;
+  mrb_float scale_first, scale_last;
+  int argc = mrb_get_args(mrb, "oo|ooff", &target, &pathwire, &cont, &corr, &scale_first, &scale_last);
+
+  TopoDS_Shape* shape_profile = siren_shape_get(mrb, target);
+  TopoDS_Shape* shape_path = siren_shape_get(mrb, pathwire);
+
+  TopoDS_Wire path;
+
+  if (shape_path->ShapeType() == TopAbs_EDGE) {
+    path = BRepBuilderAPI_MakeWire(TopoDS::Edge(*shape_path));
+  }
+  else if (shape_path->ShapeType() == TopAbs_WIRE) {
+    path = TopoDS::Wire(*shape_path);
+  }
+  else {
+    static const char m[] = "Path object is not Edge or Wire.";
+    return mrb_exc_new(mrb, E_ARGUMENT_ERROR, m, sizeof(m) - 1);
+  }
+
+  mrb_value result = mrb_nil_value();
+
+  if (argc >= 3 && argc <= 6) {
+
+    Standard_Boolean withContact = (Standard_Boolean)mrb_test(cont);
+    Standard_Boolean withCorrection = (Standard_Boolean)mrb_test(corr);
+
+    BRepOffsetAPI_MakePipeShell ps(path);
+
+    // get params
+    Standard_Real fparam, lparam;
+    {
+      BRepAdaptor_CompCurve cc(path);
+      fparam = cc.FirstParameter();
+      lparam = cc.LastParameter();
+    }
+
+    if (argc < 6) {
+      scale_last  = 1.0;
+      if (argc < 5) {
+        scale_first = 1.0;
+      }
+    }
+
+    //Handle(Law_Linear) law = new Law_Linear();
+    //law->Set(fparam, scale_first, lparam, scale_last);
+
+    Handle(Law_S) law = new Law_S();
+    law->Set(fparam, scale_first, lparam, scale_last);
+
+    //Handle(Law_Composite) law = new Law_Composite(fparam, lparam, 1.0e-6);
+
+    // get start point
+    TopoDS_Vertex pfirst; {
+      TopoDS_Vertex plast;
+      TopExp::Vertices(path, pfirst, plast);
+    }
+
+    ps.SetLaw(
+        *shape_profile, // セクションプロファイル
+        law,           // 掃引規則
+        pfirst,        // 開始点
+        withContact,   // セクションプロファイルを開始点に移動して掃引する
+        withCorrection // セクションプロファイルをパスに応じて回転する
+        );
+
+    ps.Build();
+    TopoDS_Shape* shape = new TopoDS_Shape();
+    *shape = ps.Shape();
+    result = siren_shape_new(mrb, shape);
+  }
+  else {
+    BRepOffsetAPI_MakePipe mp(path, *shape_profile);
+    mp.Build();
+    TopoDS_Shape* shape = new TopoDS_Shape();
+    *shape = mp.Shape();
+    result = siren_shape_new(mrb, shape);
+  }
+  return result;
 }
 
--- src/offset.h
+++ src/offset.h
@@ -4,11 +4,17 @@
 #include "siren.h"
 #include "shape.h"
 #include "build.h"
+#include "exp.h"
 
 #include <BRepOffsetAPI_MakePipe.hxx>
+#include <BRepOffsetAPI_MakePipeShell.hxx>
+#include <BRepAdaptor_CompCurve.hxx>
+#include <TopExp.hxx>
+#include <Law_S.hxx>
 
 bool siren_offset_install(mrb_state* mrb, struct RClass* rclass);
 
 mrb_value siren_offset_sweep_vec(mrb_state* mrb, mrb_value self);
+mrb_value siren_offset_sweep_path(mrb_state* mrb, mrb_value self);
 
 #endif
