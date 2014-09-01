688549c9b6eaa70af1f68088a18f5deb2a8496c9
dyama <dyama@member.fsf.org>
2014-09-01 14:10:34 +0900

    Add section method to Algo module.

--- src/algo.cpp
+++ src/algo.cpp
@@ -3,7 +3,31 @@
 bool siren_algo_install(mrb_state* mrb, struct RClass* rclass)
 {
   rclass = mrb_define_module(mrb, "Algo");
-  // mrb_define_class_method(mrb, rclass, "section", siren_algo_section, ARGS_REQ(2));
+  mrb_define_class_method(mrb, rclass, "section", siren_algo_section, ARGS_REQ(2));
   return true;
+}
+
+mrb_value siren_algo_section(mrb_state* mrb, mrb_value self)
+{
+  mrb_value s1, s2;
+  int argc = mrb_get_args(mrb, "oo", &s1, &s2);
+
+  TopoDS_Shape* S1 = siren_shape_get(mrb, s1);
+  TopoDS_Shape* S2 = siren_shape_get(mrb, s2);
+
+  BRepAlgoAPI_Section sect(*S1, *S2, Standard_False );
+  sect.ComputePCurveOn1(Standard_True);
+  sect.Approximation(Standard_True);
+  sect.Build();
+
+  if (!sect.IsDone()) {
+    static const char m[] = "Failed to intersection.";
+    return mrb_exc_new(mrb, E_ARGUMENT_ERROR, m, sizeof(m) - 1);
+  }
+
+  TopoDS_Shape* shape = new TopoDS_Shape();
+  *shape = sect.Shape();
+
+  return siren_shape_new(mrb, shape);
 }
 
--- src/algo.h
+++ src/algo.h
@@ -4,8 +4,10 @@
 #include "siren.h"
 #include "shape.h"
 
+#include <BRepAlgoAPI_Section.hxx>
+
 bool siren_algo_install(mrb_state* mrb, struct RClass* rclass);
 
-// mrb_value siren_algo_section(mrb_state* mrb, mrb_value self);
+mrb_value siren_algo_section(mrb_state* mrb, mrb_value self);
 
 #endif
