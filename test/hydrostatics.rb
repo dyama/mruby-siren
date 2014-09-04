c33a90defaac03f2694005dafb571723e77528b6
dyama <dyama@member.fsf.org>
2014-09-05 01:54:23 +0900

    Update test script, not running.

--- test/hydrostatics.rb
+++ test/hydrostatics.rb
@@ -9,7 +9,7 @@ no = ""
 sl = []
 t = Hash.new
 
-# Parse
+puts "Parse offset table..."
 File.open(ofsfile, "r") do |f|
   f.each do |ln|
     if ln == nil then next end
@@ -35,7 +35,7 @@ File.open(ofsfile, "r") do |f|
   end
 end
 
-# Make hullshape surface
+puts "Make hullshape surface..."
 ws = Array.new
 t.each do |x, ln|
   x = x * lpp / 10
@@ -45,10 +45,84 @@ t.each do |x, ln|
   end
   ws.push Build.polyline(pts)
 end
-res = Offset.loft ws
+hullsurf = Offset.loft ws
 
+ae_line = t.sort_by{|k,v|k}.first
+fe_line = t.sort_by{|k,v|k}.last
+zmin    = ae_line.last.sort_by{|k,v|k}.first.first
+zmax    = ae_line.last.sort_by{|k,v|k}.last.first
+xmin    = ae_line.first
+xmax    = fe_line.first * lpp / 10
 
+puts "Make XZ plane on center line..."
+cl_plane = Build.polygon [
+  Vec.new(xmin, 0, zmin), Vec.new(xmin, 0, zmax),
+  Vec.new(xmax, 0, zmax), Vec.new(xmax, 0, zmin),
+]
 
-BRepIO.save res, "/tmp/#{no}.brep"
+puts "Make XY plane on top..."
+pts = Array.new
+pts.push(Vec.new(xmin, 0, zmax))
+t.each do |x, ln|
+  pts.push(Vec.new((x * lpp / 10), ln.sort.last.last, zmax))
+end
+pts.push(Vec.new(xmax, 0, zmax))
+top_plane = Build.polygon pts
+
+# Make end plate
+def makeend(line)
+  zmin = line.last.sort_by{|k,v|k}.first.first
+  zmax = line.last.sort_by{|k,v|k}.last.first
+  xpos = line.first
+  pls = Array.new
+  pts = nil
+  line.last.sort_by{|k,v|k}.each_cons(2) do |cur,nex|
+    cz = cur.first; cy = cur.last
+    nz = nex.first; ny = nex.last
+    if pts == nil
+      # In out of polygon
+      if cz == zmin && cy > 0 # Already started at first
+        pts = Array.new
+        pts.push Vec.new xpos, 0, zmin
+        pts.push Vec.new xpos, cy, cz
+        next
+      end
+      if cy == 0 && ny > 0    # Start polygon
+        pts = Array.new
+        pts.push Vec.new xpos, cy, cz
+        next
+      end
+    else
+      # In polygon
+      pts.push Vec.new xpos, cy, cz
+      if cy == 0              # Break from polygon
+        pls.push Build.polygon pts
+        pts = nil
+      end
+    end
+  end
+  if pts != nil
+    pts.push Vec.new xpos, 0, zmax
+    pls.push Build.polygon pts
+  end
+  return pls
+end
+
+puts "Make transom and F.E. plane..."
+pl_transom = makeend ae_line
+pl_foreend = makeend fe_line
+
+comp = Build.compound [hullsurf, cl_plane, top_plane] + pl_transom + pl_foreend
+BRepIO.save comp, "/tmp/#{no}.brep"
+
+#puts "Sewing faces to shell..."
+#shell = Build.sewing [hullsurf, cl_plane, top_plane] + pl_transom + pl_foreend
+
+#puts "Build solid from shell..."
+#hullsolid = Build.solid hullsolid
+#
+#puts "Save solid model to file..."
+#BRepIO.save hullsolid, "/tmp/#{no}.brep"
+
 puts "Done."
 
