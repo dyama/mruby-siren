b42830f5fe9fcf44b6ebb5ab094c2b1a7396b621
dyama <dyama@member.fsf.org>
2014-10-28 16:50:58 +0900

    Add test script.

---
+++ test/curvature.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env siren
+# coding: utf-8
+
+# unit = 1m
+
+# 適当な構成点を与えて曲線を発生
+# (入出力ベクトルは省略)
+c = Build.curve([
+  Vec.new(0, 0, 0),     Vec.new(0, 1, 0),
+  Vec.new(0, 2, 0.5),   Vec.new(0, 3, 1.8),
+  Vec.new(0, 4, 3.8),   Vec.new(0, 5, 5.5),
+  Vec.new(0, 6, 6.8),   Vec.new(0, 6.5, 8.2),
+  Vec.new(0, 6.8, 9.5), Vec.new(0, 7, 11.2),
+])
+
+# トレランス、刻みピッチ、曲率ベクトルの大きさ係数
+t = 0.01 # 1cm
+pt = 0.2
+f = -3.0
+
+# 始終点の曲線パラメータを取得
+spr = c.param(c.sp, t)
+tpr = c.param(c.tp, t)
+
+cvs = []
+pps = []
+
+# 計算
+spr.floor.step(tpr.floor, pt) do |pr|
+  # パラメータにおける三次元位置
+  cp = c.to_xyz pr
+  # パラメータにおける曲率ベクトル
+  cv = c.curvature pr
+  # ベクトル同士の和差、スカラ積は未実装・・・
+  # pp = cp + cv * f
+  pp = Vec.new(cp.x+cv.x*f,cp.y+cv.y*f,cp.z+cv.z*f)
+  cvs.push Build.line(cp, pp)
+  pps.push pp
+end
+
+# 以下、表示
+
+world = World.new `echo $DISPLAY`.chomp
+cam = Camera.new world, X11.test
+
+s_cvs = Skin.new Build.compound cvs
+s_cvs.color = ColorName::WHITE
+world.add s_cvs
+
+s_c = Skin.new c
+s_c.color = ColorName::RED
+world.add s_c
+
+s_pl = Skin.new Build.polyline(pps)
+s_pl.color = ColorName::GREEN
+world.add s_pl
+
+# 視点・視野設定
+cam.proj = OrientType::Xneg
+cam.fitz
+cam.fit
+
+# Enter が押されたら終了
+STDIN.gets
---
+++ test/hull.rb
@@ -0,0 +1,100 @@
+#!siren
+
+# CSV offset table parser
+def get_table(ofsfile)
+  t = {}
+  ci = []
+  File::open(ofsfile, "r") do |f|
+    f.each do |ln|
+      ar = ln.chomp.split(",")
+      if ar.first == "" # Colum header
+        ar.slice(1..-1).each do |ord|
+          t[ord.to_f] = {}
+          ci.push ord.to_f
+        end
+      else
+        ar.slice(1..-1).each_with_index do |val, i|
+          t[ci[i]][ar.first.to_f] = val.to_f
+        end
+      end
+    end
+  end
+  return t
+end
+
+def make_shell(tbl, lpp)
+  cvs = []
+  tbl.each_pair do |k, v|
+    pts = []
+    x = k * lpp * 0.1
+    v.each_pair do |z, y|
+      pts.push Vec::new(x, y, z)
+    end
+    #cvs.push Build::curve(pts)
+    cvs.push Build::polyline(pts)
+  end
+  cvs.map!{|e| Build::wire [e] }
+  Offset::loft(cvs, false, false, true)
+end
+
+def get_area(tbl, lpp)
+  ar = tbl.to_a
+  aex = ar.first.first * lpp * 0.1
+  fex = ar.last.first  * lpp * 0.1
+  dmin = ar.first.last.to_a.first.first
+  dmax = ar.first.last.to_a.last.first
+  [aex, fex, dmin, dmax]
+end
+
+def make_top(tbl, lpp)
+  pts = []
+  aex, fex, dmin, dmax = get_area(tbl, lpp)
+  pts.push Vec::new(aex, 0, dmax)
+  tbl.each_pair do |k, v|
+    gnl = v.to_a.last
+    pts.push Vec::new(k * lpp * 0.1, gnl.last, gnl.first)
+  end
+  pts.push Vec::new(fex, 0, dmax)
+  Build::polygon pts
+end
+
+def make_center(tbl, lpp)
+  aex, fex, dmin, dmax = get_area(tbl, lpp)
+  Build::polygon [
+    Vec::new(aex, 0, dmin),
+    Vec::new(aex, 0, dmax),
+    Vec::new(fex, 0, dmax),
+    Vec::new(fex, 0, dmin),
+  ]
+end
+
+def make_section(xpos, curve)
+
+
+end
+
+lpp = 217.0
+table = get_table(ARGV[0])
+top    = make_top(table, lpp)
+center = make_center(table, lpp)
+shell  = make_shell(table, lpp)
+
+# 以下、表示
+world = World.new `echo $DISPLAY`.chomp
+cam = Camera.new world, X11.test
+ss = Skin.new shell
+ss.color = ColorName::RED
+world.add ss
+st = Skin.new top
+st.color = ColorName::BLUE1
+world.add st
+sc = Skin.new center
+sc.color = ColorName::GREEN
+world.add sc
+# 視点・視野設定
+cam.proj = OrientType::XnegYnegZpos
+cam.fitz
+cam.fit
+# Enter が押されたら終了
+STDIN.gets
+
