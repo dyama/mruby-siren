5c2bf2d51b1dd04f0b78cdd0116502f9ef761ed2
dyama <dyama@member.fsf.org>
2015-05-21 21:56:24 +0900

    Add method "save" to STL module.

--- inc/stl.h
+++ inc/stl.h
@@ -5,11 +5,11 @@
 #include "vec.h"
 #include "shape.h"
 
-#include <StlAPI_Reader.hxx>
+#include <StlAPI.hxx>
 
 bool siren_stl_install(mrb_state* mrb, struct RClass* rclass);
 
-// mrb_value siren_stl_save(mrb_state* mrb, mrb_value self);
+mrb_value siren_stl_save(mrb_state* mrb, mrb_value self);
 mrb_value siren_stl_load(mrb_state* mrb, mrb_value self);
 
 #endif
