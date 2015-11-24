fc6d72f00cfba9b7da1f6aa9246ae24053386c2f
dyama <dyama@member.fsf.org>
2015-11-24 16:43:34 +0900

    Fix a critical bug on Array ex class.

--- mrblib/ary.rb
+++ mrblib/ary.rb
@@ -5,13 +5,9 @@ class Array
 
   def to_v
     if size == 0
-      Vec::new 0.0, 0.0, 0.0
+      Vec::zero
     else
-      x = 0.0, y = 0.0, z = 0.0
-      if size > 0 then x = self[0].to_f end
-      if size > 1 then y = self[1].to_f end
-      if size > 2 then z = self[2].to_f end
-      Vec::new x, y, z
+      Vec::new self.x, self.y, self.z
     end
   end
 
@@ -29,11 +25,11 @@ class Array
     val.is_a?(Float) ? val : val.to_f
   end
   def y
-    val = (self[2] ||= 0.0)
+    val = (self[1] ||= 0.0)
     val.is_a?(Float) ? val : val.to_f
   end
   def z
-    val = (self[1] ||= 0.0)
+    val = (self[2] ||= 0.0)
     val.is_a?(Float) ? val : val.to_f
   end
 
