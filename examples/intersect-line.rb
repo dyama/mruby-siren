01abc06815028bb4d1a64ebf26d1ab9797de7dd8
dyama <dyama@member.fsf.org>
2015-11-26 13:14:09 +0900

    Add new example file.

---
+++ examples/intersect-line.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/siren
+# coding: utf-8
+#
+# Œğü ”­¶ƒTƒ“ƒvƒ‹
+#
+
+tor = Prim::torus [], Vec::zdir, 30, 10, Math::PI * 2
+pln = Build::infplane [], [0.5, 0.3, 0.8].to_v.normal
+int_line = tor.section(pln)
+
+faces = []
+int_line.explore(ShapeType::EDGE) do |e|
+  w = Build::wire [e]
+  faces << (Build::face w, true)
+end
+
+comp = Build::compound faces
+BRepIO::save comp, "int.brep"
+BRepIO::save tor, "torus.brep"
