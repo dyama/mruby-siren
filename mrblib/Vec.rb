35663ecb675ecd35c3b031c86aabda64c8588f5c
dyama <dyama@member.fsf.org>
2016-05-06 14:05:41 +0900

    Add new methods to Vec class.

--- mrblib/Vec.rb
+++ mrblib/Vec.rb
@@ -7,16 +7,56 @@ class Vec
     Vec.new [0.0, 0.0, 0.0]
   end
 
-  def self.xdir
+  def self.x
     Vec.new [1.0, 0.0, 0.0]
   end
 
-  def self.ydir
+  def self.y
     Vec.new [0.0, 1.0, 0.0]
   end
 
-  def self.zdir
+  def self.z
     Vec.new [0.0, 0.0, 1.0]
+  end
+
+  def self.xy
+    Vec.new [1.0, 1.0, 0.0]
+  end
+
+  def self.xz
+    Vec.new [1.0, 0.0, 1.0]
+  end
+
+  def self.yx
+    Vec.new [1.0, 1.0, 0.0]
+  end
+
+  def self.yz
+    Vec.new [0.0, 1.0, 1.0]
+  end
+
+  def self.zx
+    Vec.new [1.0, 0.0, 1.0]
+  end
+
+  def self.zy
+    Vec.new [0.0, 1.0, 1.0]
+  end
+
+  def self.xyz
+    Vec.new [1.0, 1.0, 1.0]
+  end
+
+  def self.xdir
+    Vec.x
+  end
+
+  def self.ydir
+    Vec.y
+  end
+
+  def self.zdir
+    Vec.z
   end
 
 end
