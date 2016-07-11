cfd82fbef14ac8d5b0c3740f703317f3d4cdbd3d
dyama <dyama@member.fsf.org>
2016-07-11 21:08:04 +0900

    Fix a typo.

--- examples/view/config.js
+++ examples/view/config.js
@@ -1,6 +1,6 @@
 // configuration for siren viewer
 
 var material_face = new THREE.MeshPhongMaterial({color: 'slategray'});
-material_face.side = THEE.DoubleSide;
+material_face.side = THREE.DoubleSide;
 var material_edge = new THREE.LineBasicMaterial({linewidth: 2, color: 'red'});
 
