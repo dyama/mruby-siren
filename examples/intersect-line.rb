2d075c7d21c4559a45741793c39094c739911ba8
dyama <dyama@member.fsf.org>
2016-08-24 22:40:04 +0900

    Fix the name of variable.

--- examples/intersect-line.rb
+++ examples/intersect-line.rb
@@ -4,16 +4,17 @@
 # 交線 発生サンプル
 #
 
-tor = Prim::torus [], Vec::zdir, 30, 10, Math::PI * 2
-pln = Build::infplane [], [0.5, 0.3, 0.8].to_v.normal
-int_line = tor.section(pln)
+tor = Prim.torus [0, 0, 0], Vec.zdir, 30, 10, Math::PI * 2
+pln = Build.infplane [0, 0, 0], [0.5, 0.3, 0.8].to_v.normal
+int_curve = tor.section(pln)
 
 faces = []
-int_line.explore(ShapeType::EDGE) do |e|
-  w = Build::wire [e]
-  faces << (Build::face w, true)
+int_curve.edges do |e|
+  w = Build.wire [e]
+  faces << (Build.face w, true)
 end
 
-comp = Build::compound faces
-BRepIO::save comp, "int.brep"
-BRepIO::save tor, "torus.brep"
+comp = Build.compound faces
+BRepIO.save comp, "int.brep"
+BRepIO.save tor, "torus.brep"
+
