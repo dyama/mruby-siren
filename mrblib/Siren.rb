e57810ca8f644974407e09f799e69254d7baabf4
dyama <dyama@member.fsf.org>
2016-08-27 23:30:02 +0900

    Add alias name for methods of Build, Prim, Offset and BRepIO.

--- mrblib/Siren.rb
+++ mrblib/Siren.rb
@@ -4,13 +4,54 @@
 #
 
 module Siren
-
   {
-    box: Prim,
-    copy: Build,
-    vertex: Build,
-    line: Build,
-    infline: Build,
+    # Prim package
+    box:             Prim,
+    box2p:           Prim,
+    boxax:           Prim,
+    sphere:          Prim,
+    cylinder:        Prim,
+    cone:            Prim,
+    torus:           Prim,
+    halfspace:       Prim,
+    prism:           Prim,
+    revol:           Prim,
+    revolution:      Prim,
+    wedge:           Prim,
+    # Build package
+    copy:            Build,
+    vertex:          Build,
+    line:            Build,
+    infline:         Build,
+    polyline:        Build,
+    curve:           Build,
+    wire:            Build,
+    arc:             Build,
+    arc3p:           Build,
+    circle:          Build,
+    circle3p:        Build,
+    plane:           Build,
+    face:            Build,
+    infplane:        Build,
+    polygon:         Build,
+    nurbscurve:      Build,
+    beziersurf:      Build,
+    nurbssurf:       Build,
+    sewing:          Build,
+    solid:           Build,
+    compound:        Build,
+    # BRepIO package
+    save:            BRepIO,
+    load:            BRepIO,
+    dump:            BRepIO,
+    # Offset package
+    sweep_vec:       Offset,
+    sweep_path:      Offset,
+    loft:            Offset,
+    offset_geomsurf: Offset,
+    offset:          Offset,
+    offset_shape:    Offset,
+    pipe:            Offset,
   }.each do |method, package|
     define_method(method) do |*args|
       package.send(method, *args)
@@ -19,5 +60,5 @@ module Siren
       package.send(method, *args)
     end
   end
-
 end
+
