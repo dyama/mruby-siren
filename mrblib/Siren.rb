5b137d9908a0ce61387dbfa6dff7ca9caa0c2e17
Daisuke Yamaguchi <dyama@sandbox.imi.kyushu-u.ac.jp>
2016-08-31 18:15:15 +0900

    Add save_model method to Siren module.

--- mrblib/Siren.rb
+++ mrblib/Siren.rb
@@ -19,5 +19,25 @@ module Siren
     $stderr.puts("siren: warning: " + msg.join)
   end
 
+  def self.save_model(shape, path)
+    e5 = path[-5, path.size].downcase
+    e4 = path[-4, path.size].downcase
+    if e5 == ".brep" || e4 == ".brp"
+      Siren.save_brep shape, path
+    elsif e5 == ".iges" || e4 == ".igs"
+      Siren.save_iges shape, path
+    elsif e5 == ".step" || e4 == ".stp"
+      Siren.save_step shape, path
+    elsif e4 == ".stl"
+      Siren.save_stl shape, path
+    else
+      raise TypeError
+    end
+  end
+
+  def save_model(shape, path)
+    Siren.save_model(shape, path)
+  end
+
 end
 
