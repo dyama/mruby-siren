5fc198dc3b4ffd9307e1c515694902689a80f029
dyama <dyama@member.fsf.org>
2016-09-02 09:34:14 +0900

    Update example.

--- examples/tree.rb
+++ examples/tree.rb
@@ -4,13 +4,11 @@
 # fractal tree
 #
 
-include Siren
-
 $start_len = 100.0
 
 def tree(cur_pt, dir, len)
   nxt_pt = cur_pt + dir * len
-  edges = [ line(cur_pt, nxt_pt) ]
+  edges = [ Siren.line(cur_pt, nxt_pt) ]
   len *= 0.75
   if len > $start_len / 10.0
     edges.concat tree(nxt_pt, dir.rotate(Vec.ydir, 30.0.to_rad), len)
@@ -21,8 +19,8 @@ end
 
 lines = tree([0, 0, 0].to_v, Vec.zdir, $start_len)
 
-comp = compound lines
-save_brep comp, "tree.brep"
+comp = lines.to_comp
+Siren.save_brep comp, "tree.brep"
 
 puts "done."
 
