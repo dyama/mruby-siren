22d88642cc4f57757b538c8cda81e9f35b176cc3
dyama <dyama@member.fsf.org>
2016-10-25 21:17:45 +0900

    Change type name.

--- mrblib/Curve.rb
+++ mrblib/Curve.rb
@@ -3,31 +3,31 @@
 #
 class Siren::Curve
 
-  UNKNOWN      = 0
-  BEZIERCURVE  = 1
-  BSPLINECURVE = 2
-  TRIMMEDCURVE = 3
-  BOUNDEDCURVE = 4
-  CIRCLE       = 5
-  ELLIPSE      = 6
-  HYPERBOLA    = 7
-  PARABOLA     = 8
-  CONIC        = 9
-  LINE         = 10
-  OFFSETCURVE  = 11
-  COMPLEXCURVE = 12
+  UNKNOWN   = 0
+  BEZIER    = 1
+  BSPLINE   = 2
+  TRIMMED   = 3
+  BOUNDED   = 4
+  CIRCLE    = 5
+  ELLIPSE   = 6
+  HYPERBOLA = 7
+  PARABOLA  = 8
+  CONIC     = 9
+  LINE      = 10
+  OFFSET    = 11
+  COMPLEX   = 12
 
   #
   # ジオメトリカーブタイプ(番号)をカーブ名文字列にする
   #
   def self.typename(type)
     case type
-    when Siren::Curve::BEZIERCURVE
-      return "BEZIERCURVE"
-    when Siren::Curve::BSPLINECURVE
-      return "BSPLINECURVE"
-    when Siren::Curve::TRIMMEDCURVE
-      return "TRIMMEDCURVE"
+    when Siren::Curve::BEZIER
+      return "BEZIER"
+    when Siren::Curve::BSPLINE
+      return "BSPLINE"
+    when Siren::Curve::TRIMMED
+      return "TRIMMED"
     when Siren::Curve::CIRCLE
       return "CIRCLE"
     when Siren::Curve::ELLIPSE
@@ -40,10 +40,10 @@ class Siren::Curve
       return "CONIC"
     when Siren::Curve::LINE
       return "LINE"
-    when Siren::Curve::OFFSETCURVE
-      return "OFFSETCURVE"
-    when Siren::Curve::COMPLEXCURVE
-      return "COMPLEXCURVE"
+    when Siren::Curve::OFFSET
+      return "OFFSET"
+    when Siren::Curve::COMPLEX
+      return "COMPLEX"
     else
       return "UNKNOWN"
     end
