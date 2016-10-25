8f52af4ea3cc8406ec2c7d1a264e111d092c895c
dyama <dyama@member.fsf.org>
2016-10-26 00:24:29 +0900

    Add center method to circle object.

--- inc/curve/circle.h
+++ inc/curve/circle.h
@@ -7,7 +7,14 @@
 #include <Geom_Circle.hxx>
 
 void siren_circle_install(mrb_state* mrb, RObject* o);
+Handle(Geom_Circle) siren_circle_get(mrb_state* mrb, mrb_value self);
+
 mrb_value siren_circle_radius(mrb_state* mrb, mrb_value self);
 mrb_value siren_circle_radius_set(mrb_state* mrb, mrb_value self);
+mrb_value siren_circle_center(mrb_state* mrb, mrb_value self);
+mrb_value siren_circle_center_set(mrb_state* mrb, mrb_value self);
+// mrb_value siren_circle_area(mrb_state* mrb, mrb_value self);
+// mrb_value siren_circle_axis(mrb_state* mrb, mrb_value self);
+// mrb_value siren_circle_length(mrb_state* mrb, mrb_value self);
 
 #endif
--- src/curve/circle.cpp
+++ src/curve/circle.cpp
@@ -1,32 +1,46 @@
 #include "curve/circle.h"
 
+Handle(Geom_Circle) siren_circle_get(mrb_state* mrb, mrb_value self)
+{
+  Handle(Geom_Curve) hgc = *siren_curve_get(mrb, self);
+  Handle(Geom_Circle) circle = Handle(Geom_Circle)::DownCast(hgc);
+  if (circle.IsNull()) {
+    mrb_raise(mrb, E_RUNTIME_ERROR, "The geometry type is not circle.");
+  }
+  return circle;
+}
+
 void siren_circle_install(mrb_state* mrb, RObject* o)
 {
   mrb_define_singleton_method(mrb, o, "radius",  siren_circle_radius, MRB_ARGS_NONE());
   mrb_define_singleton_method(mrb, o, "radius=", siren_circle_radius_set, MRB_ARGS_REQ(1));
+  mrb_define_singleton_method(mrb, o, "center",  siren_circle_center, MRB_ARGS_NONE());
+  mrb_define_singleton_method(mrb, o, "center=", siren_circle_center_set, MRB_ARGS_REQ(1));
   return;
 }
 
 mrb_value siren_circle_radius(mrb_state* mrb, mrb_value self)
 {
-  Handle(Geom_Curve) hgc = *siren_curve_get(mrb, self);
-  Handle(Geom_Circle) circle = Handle(Geom_Circle)::DownCast(hgc);
-  if (circle.IsNull()) {
-    mrb_raise(mrb, E_RUNTIME_ERROR, "The geometry type is not circle.");
-  }
-  Standard_Real r = circle->Radius();
+  Standard_Real r = siren_circle_get(mrb, self)->Radius();
   return mrb_float_value(mrb, r);
 }
 
 mrb_value siren_circle_radius_set(mrb_state* mrb, mrb_value self)
 {
-  Handle(Geom_Curve) hgc = *siren_curve_get(mrb, self);
-  Handle(Geom_Circle) circle = Handle(Geom_Circle)::DownCast(hgc);
-  if (circle.IsNull()) {
-    mrb_raise(mrb, E_RUNTIME_ERROR, "The geometry type is not circle.");
-  }
   mrb_float r;
   int argc = mrb_get_args(mrb, "f", &r);
-  circle->SetRadius(r);
+  siren_circle_get(mrb, self)->SetRadius(r);
   return mrb_nil_value();
 }
+
+mrb_value siren_circle_center(mrb_state* mrb, mrb_value self)
+{
+  gp_Pnt center = siren_circle_get(mrb, self)->Circ().Location();
+  return siren_pnt_to_ary(mrb, center);
+}
+
+mrb_value siren_circle_center_set(mrb_state* mrb, mrb_value self)
+{
+  return mrb_nil_value();
+}
+
