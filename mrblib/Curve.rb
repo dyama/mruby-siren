1e7972514575b8f4602d9a2a3338a6c6fcd91005
dyama <dyama@member.fsf.org>
2016-10-26 09:51:53 +0900

    Add type check methods to curve class.

--- mrblib/Curve.rb
+++ mrblib/Curve.rb
@@ -28,6 +28,8 @@ class Siren::Curve
       return "BSPLINE"
     when Siren::Curve::TRIMMED
       return "TRIMMED"
+    when Siren::Curve::BOUNDED
+      return "BOUNDED"
     when Siren::Curve::CIRCLE
       return "CIRCLE"
     when Siren::Curve::ELLIPSE
@@ -48,5 +50,24 @@ class Siren::Curve
       return "UNKNOWN"
     end
   end
+
+  def bezier?;    self.type == Siren::Curve::BEZIER    end
+  def bspline?;   self.type == Siren::Curve::BSPLINE   end
+  def trimmed?;   self.type == Siren::Curve::TRIMMED   end
+  def bounded?
+    self.type == Siren::Curve::BOUNDED ||
+      self.bezier? || self.bspline? || self.trimmed?
+  end
+  def circle?;    self.type == Siren::Curve::CIRCLE    end
+  def ellipse?;   self.type == Siren::Curve::ELLIPSE   end
+  def hyperbola?; self.type == Siren::Curve::HYPERBOLA end
+  def parabola?;  self.type == Siren::Curve::PARABOLA  end
+  def conic?
+    self.type == Siren::Curve::CONIC ||
+      self.circle? || self.ellipse? || self.hyperbola? || self.parabola?
+  end
+  def line?;      self.type == Siren::Curve::LINE      end
+  def offset?;    self.type == Siren::Curve::OFFSET    end
+  def complex?;   self.type == Siren::Curve::COMPLEX   end
 
 end
