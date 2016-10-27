2b6e40c3d1aaf177d4a74e2ecaebc00ba8ce2c2e
dyama <dyama@member.fsf.org>
2016-10-27 18:20:58 +0900

    Add dir method to Line class.

--- src/curve/line.cpp
+++ src/curve/line.cpp
@@ -33,6 +33,8 @@ bool siren_line_install(mrb_state* mrb, struct RClass* mod_siren)
   struct RClass* cls_line = mrb_define_class_under(mrb, mod_siren, "Line", cls_curve);
   MRB_SET_INSTANCE_TT(cls_line, MRB_TT_DATA);
   mrb_define_method(mrb, cls_line, "initialize", siren_curve_init, MRB_ARGS_NONE());
+
+  mrb_define_method(mrb, cls_line, "dir", siren_line_dir, MRB_ARGS_NONE());
   return true;
 }
 
@@ -40,5 +42,11 @@ struct RClass* siren_line_rclass(mrb_state* mrb)
 {
   struct RClass* mod_siren = mrb_module_get(mrb, "Siren");
   return mrb_class_ptr(mrb_const_get(mrb, mrb_obj_value(mod_siren), mrb_intern_lit(mrb, "Line")));
+}
+
+mrb_value siren_line_dir(mrb_state* mrb, mrb_value self)
+{
+  Handle(Geom_Line) line = siren_line_get(mrb, self);
+  return siren_dir_to_ary(mrb, line->Lin().Direction());
 }
 
