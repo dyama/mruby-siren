a146c3b189e369706ade2513a2b32ad2d7580fac
dyama <dyama@member.fsf.org>
2016-10-31 21:07:39 +0900

    Update example.

--- examples/make-hull.rb
+++ examples/make-hull.rb
@@ -39,33 +39,33 @@ av = [(pp_aft - ae) / 4.0, b2].to_v - [ae, b4].to_v
 av.normal!
 ptsa = [ [ae, b4, d], [pp_aft - (pp_aft - ae) / 2.0, b2, d]]
 vcsa = [ av.to_a, [pp_len / 2.0, 0.0]]
-gunnela = curve(ptsa, vcsa)
+gunnela = interpolate(ptsa, vcsa)
 lines << gunnela
 
 # フォアガンネルライン
 fv = [xtop_max - pp_fore, 0].to_v - [(xtop_max - pp_fore) * 3.0 / 4.0, b2].to_v
 ptsf = [[pp_fore + (xtop_max - pp_fore) / 3.0, b2, d], [xtop_max, 0, d] ]
 vcsf = [ [pp_len / 2.0, 0.0], fv.to_a ]
-gunnelf = curve(ptsf, vcsf)
+gunnelf = interpolate(ptsf, vcsf)
 lines << gunnelf
 
 # サイドデッドフラットライン
 sdfpa = [pp_aft - (pp_aft - ae) / 2.0, b2, d]
 sdfpf = [pp_fore + (xtop_max - pp_fore) / 3.0, b2, d]
-sdfla = curve [sdfpa, [pp_aft - (pp_aft-ae) / 3.0, b2, d / 2.0], [pp_aft, b2, br]], [[0,0,-1],nil,[1,0,0]]
-sdflf = curve [[pp_fore, b2, br], sdfpf], [[1,0,0],[0,0,1]]
+sdfla = interpolate [sdfpa, [pp_aft - (pp_aft-ae) / 3.0, b2, d / 2.0], [pp_aft, b2, br]], [[0,0,-1],nil,[1,0,0]]
+sdflf = interpolate [[pp_fore, b2, br], sdfpf], [[1,0,0],[0,0,1]]
 lines << sdfla << sdflf
 
 # ボトムデッドフラットライン
 blae = [-ae + -ae / 5.0, 0, 0]
 blfe = [lpp - (loa-lpp)/2.0, 0, 0]
-bdfla = curve [blae, [pp_aft, b2 - br, 0]], [[1,0,0],[1,0,0]]
-bdflf = curve [[pp_fore, b2 - br, 0], blfe], [[1,0,0],[1,-0.5,0]]
+bdfla = interpolate [blae, [pp_aft, b2 - br, 0]], [[1,0,0],[1,0,0]]
+bdflf = interpolate [[pp_fore, b2 - br, 0], blfe], [[1,0,0],[1,-0.5,0]]
 lines << bdfla << bdflf
 
 # トランサム
 tsom_tl = line([ae, 0, d], [ae, b4, d])
-tsom_bl = curve [[ae, 0, d * 2.0 / 3.0], [ae, b4, d]], [[0,1,0], [0,0,1]]
+tsom_bl = interpolate [[ae, 0, d * 2.0 / 3.0], [ae, b4, d]], [[0,1,0], [0,0,1]]
 lines << tsom_tl << tsom_bl
 
 # アフト プロファイル
@@ -75,7 +75,7 @@ v2 = v1.rotate(Vec.ydir, 90.0.to_rad)
 v2.normal!
 tp = line [ae, 0, d],[ae, 0, d * 2.0 / 3.0]
 tp2 = line [ae, 0, d * 2.0 / 3.0], [-ae, 0, d / 2.0]
-tp3 = curve [[-ae, 0, d / 2.0], [-ae + -ae / 5.0, 0, d / 4.0]], [v1.to_a, v2.to_a]
+tp3 = interpolate [[-ae, 0, d / 2.0], [-ae + -ae / 5.0, 0, d / 4.0]], [v1.to_a, v2.to_a]
 tp4 = line [-ae + -ae / 5.0, 0, d / 4.0], blae
 aft_profile = wire [tp, tp2, tp3, tp4], 0.01
 lines << aft_profile
@@ -86,7 +86,7 @@ p2 = [loa, 0, d * 0.3]
 p3 = [lpp, 0, d / 4.0 * 2.5]
 p4 = [lpp, 0, d / 4.0 * 3.0]
 p5 = [xtop_max, 0, d]
-fore_profile = curve [p1, p2, [xtop_max,0,d/2.0], p3, p4, p5], [[1,0,0], [0,0,1], nil, [0,0,1], [0,0,1], (p5.to_v - p4.to_v).to_a]
+fore_profile = interpolate [p1, p2, [xtop_max,0,d/2.0], p3, p4, p5], [[1,0,0], [0,0,1], nil, [0,0,1], [0,0,1], (p5.to_v - p4.to_v).to_a]
 lines << fore_profile
 
 # ボトム面(アフト)
@@ -145,8 +145,8 @@ end
 
 # 出力
 comp = compound(lines)
-save_brep comp, "curve.brep"
+save_brep comp, "curves.brep"
 
 comp = compound(faces)
-save_brep comp, "face.brep"
+save_brep comp, "faces.brep"
 
