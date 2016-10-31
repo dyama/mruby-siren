9ee80fdcde6ef70ce1d8777c8fa9dc06452c90ca
dyama <dyama@member.fsf.org>
2016-10-31 11:09:09 +0900

    Add Siren::Solid class.

--- inc/shape/solid.h
+++ inc/shape/solid.h
@@ -6,6 +6,9 @@
 #include <TopoDS.hxx>
 #include <TopoDS_Solid.hxx>
 
-void siren_solid_install(mrb_state* mrb, RObject* o);
+static struct mrb_data_type siren_solid_type = { "Solid", siren_shape_final };
+bool siren_solid_install(mrb_state* mrb, struct RClass* mod_siren);
+TopoDS_Solid siren_solid_get(mrb_state* mrb, mrb_value self);
+mrb_value siren_solid_new(mrb_state* mrb, const TopoDS_Shape* src);
 
 #endif
--- src/shape/solid.cpp
+++ src/shape/solid.cpp
@@ -1,7 +1,39 @@
 #include "shape/solid.h"
 
-void siren_solid_install(mrb_state* mrb, RObject* o)
+mrb_value siren_solid_new(mrb_state* mrb, const TopoDS_Shape* src)
 {
-  return;
+  mrb_value obj;
+  struct RClass* cls_shape = siren_shape_rclass(mrb);
+  struct RClass* mod_siren = mrb_module_get(mrb, "Siren");
+  obj = mrb_instance_alloc(mrb, mrb_const_get(mrb, mrb_obj_value(mod_siren), mrb_intern_lit(mrb, "Solid")));
+  void* p = mrb_malloc(mrb, sizeof(TopoDS_Shape));
+  TopoDS_Shape* inner = new(p) TopoDS_Shape();
+  *inner = *src; // Copy to inner native member
+  DATA_PTR(obj)  = const_cast<TopoDS_Shape*>(inner);
+  DATA_TYPE(obj) = &siren_solid_type;
+  return obj;
+}
+
+TopoDS_Solid siren_solid_get(mrb_state* mrb, mrb_value self)
+{
+  TopoDS_Shape* shape = static_cast<TopoDS_Shape*>(mrb_get_datatype(mrb, self, &siren_solid_type));
+  TopoDS_Solid solid = TopoDS::Solid(*shape);
+  if (solid.IsNull()) { mrb_raise(mrb, E_RUNTIME_ERROR, "The geometry type is not Solid."); }
+  return solid;
+}
+
+bool siren_solid_install(mrb_state* mrb, struct RClass* mod_siren)
+{
+  struct RClass* cls_shape = siren_shape_rclass(mrb);
+  struct RClass* cls_solid = mrb_define_class_under(mrb, mod_siren, "Solid", cls_shape);
+  MRB_SET_INSTANCE_TT(cls_solid, MRB_TT_DATA);
+  mrb_define_method(mrb, cls_solid, "initialize", siren_shape_init,  MRB_ARGS_NONE());
+  return true;
+}
+
+struct RClass* siren_solid_rclass(mrb_state* mrb)
+{
+  struct RClass* mod_siren = mrb_module_get(mrb, "Siren");
+  return mrb_class_ptr(mrb_const_get(mrb, mrb_obj_value(mod_siren), mrb_intern_lit(mrb, "Solid")));
 }
 
